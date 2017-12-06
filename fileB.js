824164ff67004b892b3d08b82065c383fc7e8431
Dinesh <flexdinesh@gmail.com>
2017-12-06 23:08:07 +0530

    file B changes

--- fileB.js
+++ fileB.js
@@ -1,2 +1,2 @@
-console.log('Hey, I\'m file B!');
-console.log('adding line 2 in file B');
+console.log('Hey, I\'m file B! - editing line 1');
+console.log('adding line 2 in file B - editing line 2');
